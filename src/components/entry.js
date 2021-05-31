ed55622c488afb5e6419f1b54f1f82eff8dc17d9
Sai Jaswanth Gudibandi <jaswanth2k@gmail.com>
2021-05-31 22:22:26 +0530

    added img

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import firebase from "firebase/app";
 import "firebase/storage";
 import styled from 'styled-components'
-import { DetailsSharp } from '@material-ui/icons';
 
 const Div = styled.div `
-background-image: url(${props => props.imageurl});
+background-image: url(${props => props.url});
 height: 300px;
+width: 300px;
+margin: 0px auto;
+border-radius: 15px;
 background-position: center;
 background-size: cover;
 background-repeat: no-repeat;
@@ -14,21 +16,21 @@ background-repeat: no-repeat;
 
 function EntryComponent(props)
 {
+    const [imgurl, setimgurl] = useState()
     var storageref = firebase.storage().ref()
     const userid = sessionStorage.getItem("uid")
     if(props.entry)
     {
-        const details = props.entry
-        const basepath = `userimages/${userid}/`
-        const imageurlgetter= () =>
-        {const finalpath = basepath.concat(details.imgname)
+        const details = props.entry  
+            const basepath = `userimages/${userid}/`
+            const finalpath = basepath.concat(details.imgname)
             console.log(finalpath)
             var starsref = storageref.child(finalpath)
             starsref.getDownloadURL().then((url) => {
                 console.log(url)
-                document.getElementById(details.id).setAttribute("imageurl", url)
+                setimgurl(url)
             })
-        }
+
         console.log(props.entry)
         return (
             <div className='Ent-wrapper'>
@@ -36,9 +38,9 @@ function EntryComponent(props)
                     <h1 className='Ent-Head'>
                         {details.title}
                     </h1>
-                    <div imageurl = {imageurlgetter()}>
-                        <image src='' id = {details.id} />
-                    </div>
+                    <Div id={details.id} url = {imgurl}>
+                        {/* Just a placeholder for the image*/}
+                    </Div>
                     <p className='Ent-p'>
                         {details.entry}
                     </p>
