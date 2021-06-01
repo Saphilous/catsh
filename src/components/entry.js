f80e26e2d490d2f6fe27d8f7f768e1fb8e46040d
Sai Jaswanth Gudibandi <jaswanth2k@gmail.com>
2021-06-01 20:45:15 +0530

    failind cors for iam token

--- src/components/entry.js
+++ src/components/entry.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import firebase from "firebase/app";
 import "firebase/storage";
 import styled from 'styled-components'
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
 
 const Div = styled.div `
 background-image: url(${props => props.url});
@@ -17,8 +20,20 @@ background-repeat: no-repeat;
 function EntryComponent(props)
 {
     const [imgurl, setimgurl] = useState()
+
+    const naturallanguageunderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: 'UNL1ShV-c2bAwfGGyqQFjE-qVOOE-yPskbDybk8CWro0',
+        }),
+        serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1bcae669-efa9-4160-9535-18d3807000a3',
+    })
+    console.log(naturallanguageunderstanding)
+
     var storageref = firebase.storage().ref()
+
     const userid = sessionStorage.getItem("uid")
+    
     if(props.entry)
     {
         const details = props.entry  
@@ -30,6 +45,32 @@ function EntryComponent(props)
                 console.log(url)
                 setimgurl(url)
             })
+
+        const analyzeparams = {
+            'text': details.entry,
+            'features': {
+                'entities': {
+                    'emotion': true,
+                    'sentiment': true,
+                    'mentions': true,
+                    'limit': 10
+                },
+                'keywords': {
+                    'emotion': true,
+                    'sentiment': true,
+                    'limit': 3
+                },
+                'summarization': {
+                    'limit': 5
+                }
+            }
+        }
+
+        naturallanguageunderstanding.analyze(analyzeparams).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
 
         console.log(props.entry)
         return (
